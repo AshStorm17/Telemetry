268c71bb67b63612f0c66c14fd867ad4122dc9f5
JaidevSK <134712750+JaidevSK@users.noreply.github.com>
2025-03-20 17:56:20 +0530

    Add files via upload

--- Trials/topology.py
+++ Trials/topology.py
@@ -10,10 +10,15 @@ import time
 class SingleSwitchTopo(Topo):
     "Single switch connected to n hosts."
     def build(self, n=3):
-        switch = self.addSwitch('s1')
-        for h in range(n):
-            host = self.addHost('h%s' % (h + 1), cpu=.6/n)
-            self.addLink(host, switch, bw=500, delay='10ms', loss=0, max_queue_size=100, use_htb=True)
+        s1 = self.addSwitch('s1')
+        s1h = self.addHost('s1h')
+        h1 = self.addHost('h1')
+        cc = self.addHost('cc')
+        h2 = self.addHost('h2')
+        self.addLink(s1h, s1)
+        self.addLink(h1, s1)
+        self.addLink(cc, s1)
+        self.addLink(h2, s1)
 
 def simpleTest():
     "Create and test a simple network"
@@ -21,26 +26,21 @@ def simpleTest():
     net = Mininet(topo=topo, link=TCLink)
     net.start()
     
+    s1h = net.get('s1h')
     h1 = net.get('h1')
+    cc = net.get('cc')
     h2 = net.get('h2')
-    h3 = net.get('h3')
     s1 = net.get('s1')
 
-    # Set MAC address for h1's interface to simulate s1's MAC
-    h1.setMAC('00:00:00:00:00:01', intf='h1-eth0')
+    cc.cmd('tcpdump -i any -w received_packets.pcap &')
 
-    # Start a listener on h3 to receive packets
-    h3.cmd('tcpdump -i any -w received_packets.pcap &')
-
-    # Send packets from h1 (simulating s1) to h3 with h1's MAC address
     for _ in range(5):  
-        h1.cmd('python3 -c "from scapy.all import *; sendp(Ether(src=\'00:00:00:00:00:01\', dst=\'' + h3.MAC() + '\')/Raw(load=\'Hello\'), iface=\'h1-eth0\')"')
+        s1h.cmd('python3 -c "from scapy.all import *; sendp(Ether(src=\'' + s1h.MAC() + '\', dst=\'' + cc.MAC() + '\')/Raw(load=\'Hello\'), iface=\'s1h-eth0\')"')
         time.sleep(1)
 
-    # Running iperf test as before
-    h1.cmd('iperf -s -u -i 1 > iperf_server_output &')
+    h2.cmd('iperf -s -u -i 1 > iperf_server_output &')
     time.sleep(1)
-    h2.cmd('iperf -c ' + h1.IP() + ' -u -t 3 -b 10m')
+    h1.cmd('iperf -c ' + h2.IP() + ' -u -t 3 -b 10m')
     time.sleep(1)
 
     net.stop()
