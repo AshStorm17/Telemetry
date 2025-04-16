e68b92e3270c26d5372750e5c64c280ce376b888
VannshJani <141410835+VannshJani@users.noreply.github.com>
2025-04-16 14:31:21 +0530

    Update m_scaled.py

--- network/m_scaled.py
+++ network/m_scaled.py
@@ -10,7 +10,6 @@ import datetime
 from scapy.all import Ether, UDP, Raw, sendp
 import socket
 from health_switch import HealthMonitoringSwitch
-from packet_processor import end2end_cc2dc
 from hlp_switch import *
 from hlp_router import *
 from health_router import HealthMonitoringRouter
@@ -39,65 +38,75 @@ class CustomTopology(Topo):
 
 
         r1 = self.addHost('r1', cls=HealthMonitoringRouter) 
-        r2 = self.addHost('r2', cls=HealthMonitoringRouter) 
+        r2 = self.addHost('r2', cls=HealthMonitoringRouter)
+        # r3 = self.addHost('r3', cls=HealthMonitoringRouter) 
 
         # --- Cluster 1 Infrastructure (Subnet: 10.0.1.0/24) ---
         s1_cc1 = self.addSwitch('s1_cc1', cls = HealthMonitoringSwitch)
         s2_cc1 = self.addSwitch('s2_cc1', cls = HealthMonitoringSwitch)
-        s1_cc1_h = self.addHost('s1_cc1_h', ip='10.0.1.10/24', defaultRoute='via 10.0.1.254')
-        s2_cc1_h = self.addHost('s2_cc1_h', ip='10.0.1.11/24', defaultRoute='via 10.0.1.254')
+        s1_cc1_h = self.addHost('s1_cc1_h', ip='10.0.1.10/24', defaultRoute='via 10.0.1.1')
+        s2_cc1_h = self.addHost('s2_cc1_h', ip='10.0.1.11/24', defaultRoute='via 10.0.1.1')
 
         self.addLink(cc1, s1_cc1, intfName1='cc1-eth0')
         self.addLink(s1_cc1_h, s1_cc1)
         self.addLink(s2_cc1_h, s2_cc1)
-
-        self.addLink(s1_cc1, r1, intfName2='r1-eth0', params2={'ip': '10.0.1.254/24'})
         self.addLink(s1_cc1, s2_cc1) # Connect switches within cluster
-        # Add regular hosts
         for i in range(1, 9):
-             host = self.addHost(f'h{i}_cc1', ip=f'10.0.1.{i+1}/24', defaultRoute='via 10.0.1.254')
-             if i <= 4: self.addLink(host, s1_cc1)
-             else: self.addLink(host, s2_cc1)
+            host = self.addHost(f'h1{i}', ip=f'10.0.1.{i+11}/24')
+            if i<=4:
+                self.addLink(host, s1_cc1)
+            else:
+                self.addLink(host, s2_cc1)
 
+        self.addLink(r1, s1_cc1, intfName2='r1-eth0')
+        self.addLink(r1, s2_cc1, intfName2='r1-eth1')
+        
+        
         # --- Cluster 2 Infrastructure (Subnet: 10.0.2.0/24) ---
         s1_cc2 = self.addSwitch('s1_cc2', cls = HealthMonitoringSwitch)
         s2_cc2 = self.addSwitch('s2_cc2', cls = HealthMonitoringSwitch)
-        # These hosts need a default route via the router on their segment (use r1's IP for consistency)
-        s1_cc2_h = self.addHost('s1_cc2_h', ip='10.0.2.10/24', defaultRoute='via 10.0.2.254')
-        s2_cc2_h = self.addHost('s2_cc2_h', ip='10.0.2.11/24', defaultRoute='via 10.0.2.254')
-        # Link cc2 (eth0) and telemetry hosts to local switches
+        s1_cc2_h = self.addHost('s1_cc2_h', ip='10.0.2.10/24', defaultRoute='via 10.0.2.1')
+        s2_cc2_h = self.addHost('s2_cc2_h', ip='10.0.2.11/24', defaultRoute='via 10.0.2.1')
+
         self.addLink(cc2, s1_cc2, intfName1='cc2-eth0')
         self.addLink(s1_cc2_h, s1_cc2)
         self.addLink(s2_cc2_h, s2_cc2)
-        # Link Switches (s1 to routers, s2 to s1)
-        self.addLink(s1_cc2, r1, intfName2='r1-eth1', params2={'ip': '10.0.2.254/24'}) # Connect CC2 switch to R1
-        self.addLink(s1_cc2, r2, intfName2='r2-eth0', params2={'ip': '10.0.2.253/24'}) # Connect CC2 switch to R2
         self.addLink(s1_cc2, s2_cc2) # Connect switches within cluster
-        # Add regular hosts
         for i in range(1, 9):
-             # Default via R1's IP on this segment (10.0.2.254)
-             host = self.addHost(f'h{i}_cc2', ip=f'10.0.2.{i+1}/24', defaultRoute='via 10.0.2.254')
-             if i <= 4: self.addLink(host, s1_cc2)
-             else: self.addLink(host, s2_cc2)
+            host = self.addHost(f'h2{i}', ip=f'10.0.2.{i+11}/24')
+            if i<=4:
+                self.addLink(host, s1_cc2)
+            else:
+                self.addLink(host, s2_cc2)
+
+        self.addLink(r1, s1_cc2, intfName2='r1-eth2')
+        self.addLink(r1, s2_cc2, intfName2='r1-eth3')
+        self.addLink(r2, s1_cc2, intfName2='r2-eth0')
+        self.addLink(r2, s2_cc2, intfName2='r2-eth1')
+        
+
 
         # --- Cluster 3 Infrastructure (Subnet: 10.0.3.0/24) ---
         s1_cc3 = self.addSwitch('s1_cc3', cls = HealthMonitoringSwitch)
         s2_cc3 = self.addSwitch('s2_cc3', cls = HealthMonitoringSwitch)
-        # These hosts need a default route via the router on their segment
-        s1_cc3_h = self.addHost('s1_cc3_h', ip='10.0.3.10/24', defaultRoute='via 10.0.3.254')
-        s2_cc3_h = self.addHost('s2_cc3_h', ip='10.0.3.11/24', defaultRoute='via 10.0.3.254')
-        # Link cc3 (eth0) and telemetry hosts to local switches
+        s1_cc3_h = self.addHost('s1_cc3_h', ip='10.0.3.10/24', defaultRoute='via 10.0.3.1')
+        s2_cc3_h = self.addHost('s2_cc3_h', ip='10.0.3.11/24', defaultRoute='via 10.0.3.1')
+
         self.addLink(cc3, s1_cc3, intfName1='cc3-eth0')
         self.addLink(s1_cc3_h, s1_cc3)
         self.addLink(s2_cc3_h, s2_cc3)
-        # Link Switches (s1 to router, s2 to s1)
-        self.addLink(s1_cc3, r2, intfName2='r2-eth1', params2={'ip': '10.0.3.254/24'}) # Connect CC3 switch to R2
         self.addLink(s1_cc3, s2_cc3) # Connect switches within cluster
-        # Add regular hosts
         for i in range(1, 9):
-             host = self.addHost(f'h{i}_cc3', ip=f'10.0.3.{i+1}/24', defaultRoute='via 10.0.3.254')
-             if i <= 4: self.addLink(host, s1_cc3)
-             else: self.addLink(host, s2_cc3)
+            host = self.addHost(f'h3{i}', ip=f'10.0.3.{i+11}/24')
+            if i<=4:
+                self.addLink(host, s1_cc3)
+            else:
+                self.addLink(host, s2_cc3)
+        
+        self.addLink(r2, s1_cc3, intfName2='r2-eth2')
+        self.addLink(r2, s2_cc3, intfName2='r2-eth3')
+
+        
 
         # --- Connect Cluster Centers to Central DC Switch (dcs1) ---
         self.addLink(cc1, dcs1, intfName1='cc1-eth1', params1={'ip': '10.0.100.11/24'})
@@ -120,6 +129,7 @@ def simpleTest():
     cc3 = net.get('cc3')
     r1 = net.get('r1')
     r2 = net.get('r2')
+    # r3 = net.get('r3')
 
     # Switch-Associated Hosts - Cluster 1
     s1_cc1_h = net.get('s1_cc1_h')
@@ -127,14 +137,14 @@ def simpleTest():
     s1_cc1 = net.get("s1_cc1")
     s2_cc1 = net.get("s2_cc1")
     # Regular Hosts - Cluster 1
-    h1_cc1 = net.get('h1_cc1')
-    h2_cc1 = net.get('h2_cc1')
-    h3_cc1 = net.get('h3_cc1')
-    h4_cc1 = net.get('h4_cc1')
-    h5_cc1 = net.get('h5_cc1')
-    h6_cc1 = net.get('h6_cc1')
-    h7_cc1 = net.get('h7_cc1')
-    h8_cc1 = net.get('h8_cc1')
+    h1_cc1 = net.get('h11')
+    h2_cc1 = net.get('h12')
+    h3_cc1 = net.get('h13')
+    h4_cc1 = net.get('h14')
+    h5_cc1 = net.get('h15')
+    h6_cc1 = net.get('h16')
+    h7_cc1 = net.get('h17')
+    h8_cc1 = net.get('h18')
 
     # Switch-Associated Hosts - Cluster 2
     s1_cc2_h = net.get('s1_cc2_h')
@@ -142,14 +152,14 @@ def simpleTest():
     s1_cc2 = net.get("s1_cc2")
     s2_cc2 = net.get("s2_cc2")
     # Regular Hosts - Cluster 2
-    h1_cc2 = net.get('h1_cc2')
-    h2_cc2 = net.get('h2_cc2')
-    h3_cc2 = net.get('h3_cc2')
-    h4_cc2 = net.get('h4_cc2')
-    h5_cc2 = net.get('h5_cc2')
-    h6_cc2 = net.get('h6_cc2')
-    h7_cc2 = net.get('h7_cc2')
-    h8_cc2 = net.get('h8_cc2')
+    h1_cc2 = net.get('h21')
+    h2_cc2 = net.get('h22')
+    h3_cc2 = net.get('h23')
+    h4_cc2 = net.get('h24')
+    h5_cc2 = net.get('h25')
+    h6_cc2 = net.get('h26')
+    h7_cc2 = net.get('h27')
+    h8_cc2 = net.get('h28')
 
     # Switch-Associated Hosts - Cluster 3
     s1_cc3_h = net.get('s1_cc3_h')
@@ -157,51 +167,79 @@ def simpleTest():
     s1_cc3 = net.get("s1_cc3")
     s2_cc3 = net.get("s2_cc3")
     # Regular Hosts - Cluster 3
-    h1_cc3 = net.get('h1_cc3')
-    h2_cc3 = net.get('h2_cc3')
-    h3_cc3 = net.get('h3_cc3')
-    h4_cc3 = net.get('h4_cc3')
-    h5_cc3 = net.get('h5_cc3')
-    h6_cc3 = net.get('h6_cc3')
-    h7_cc3 = net.get('h7_cc3')
-    h8_cc3 = net.get('h8_cc3')
+    h1_cc3 = net.get('h31')
+    h2_cc3 = net.get('h32')
+    h3_cc3 = net.get('h33')
+    h4_cc3 = net.get('h34')
+    h5_cc3 = net.get('h35')
+    h6_cc3 = net.get('h36')
+    h7_cc3 = net.get('h37')
+    h8_cc3 = net.get('h38')
     
     for cc in [cc1, cc2, cc3]:
         cc.cmd('sysctl net.ipv4.ip_forward=1')
+    for r in [r1,r2]:
+        r.cmd('sysctl -w net.ipv4.ip_forward=1')
+
 
     # 2. Ensure Router Interfaces are UP
 
-    r1.cmd('ip link set r1-eth0 up') # To CC1 Switch (s1_cc1)
-    r1.cmd('ip link set r1-eth1 up') # To CC2 Switch (s1_cc2)
-    r2.cmd('ip link set r2-eth0 up') # To CC2 Switch (s1_cc2)
-    r2.cmd('ip link set r2-eth1 up') # To CC3 Switch (s1_cc3)
+    r1.cmd('ip link set r1-eth0 up')
+    r1.cmd('ip link set r1-eth1 up')
+    r1.cmd('ip link set r1-eth2 up') 
+    r1.cmd('ip link set r1-eth3 up')
 
-    # 3. Add Static Routes on Routers (r1, r2) for Inter-Cluster Traffic
-    # R1 needs route to CC3 network (10.0.3.0/24) via R2's IP on the shared CC2 segment (10.0.2.253)
-    r1.cmd('ip route add 10.0.3.0/24 via 10.0.2.253 dev r1-eth1')
-    # R2 needs route to CC1 network (10.0.1.0/24) via R1's IP on the shared CC2 segment (10.0.2.254)
-    r2.cmd('ip route add 10.0.1.0/24 via 10.0.2.254 dev r2-eth0')
+    r2.cmd('ip link set r2-eth0 up')
+    r2.cmd('ip link set r2-eth1 up')
+    r2.cmd('ip link set r2-eth2 up')
+    r2.cmd('ip link set r2-eth3 up') 
 
-    # cc1: Needs routes to cc2 and cc3 via r1 (10.0.1.254) on its local interface (cc1-eth0)
-    cc1.cmd('ip route add 10.0.2.0/24 via 10.0.1.254 dev cc1-eth0')
-    cc1.cmd('ip route add 10.0.3.0/24 via 10.0.1.254 dev cc1-eth0') # r1 knows how to reach 10.0.3.0/24
+    r1.cmd('ifconfig r1-eth0 10.0.1.200/24')
+    r1.cmd('ifconfig r1-eth1 10.0.1.201/24')
+    r1.cmd('ifconfig r1-eth2 10.0.2.200/24')
+    r1.cmd('ifconfig r1-eth3 10.0.2.201/24')
 
-    # cc2: Needs routes to cc1 (via r1 @ 10.0.2.254) and cc3 (via r2 @ 10.0.2.253) on local interface (cc2-eth0)
-    cc2.cmd('ip route add 10.0.1.0/24 via 10.0.2.254 dev cc2-eth0')
-    cc2.cmd('ip route add 10.0.3.0/24 via 10.0.2.253 dev cc2-eth0')
+    r2.cmd('ifconfig r2-eth0 10.0.2.202/24')
+    r2.cmd('ifconfig r2-eth1 10.0.2.203/24')
+    r2.cmd('ifconfig r2-eth2 10.0.3.200/24')
+    r2.cmd('ifconfig r2-eth3 10.0.3.201/24')
 
-    # cc3: Needs routes to cc1 and cc2 via r2 (10.0.3.254) on local interface (cc3-eth0)
-    cc3.cmd('ip route add 10.0.2.0/24 via 10.0.3.254 dev cc3-eth0')
-    cc3.cmd('ip route add 10.0.1.0/24 via 10.0.3.254 dev cc3-eth0') # r2 knows how to reach 10.0.1.0/24
 
-    # 5. Add Static Routes on DC Host
-    # info("Adding static routes on dc...\n")
-    dc.cmd('ip route add 10.0.1.0/24 via 10.0.100.11') # Reach CC1 via cc1's DC-facing IP
-    dc.cmd('ip route add 10.0.2.0/24 via 10.0.100.12') # Reach CC2 via cc2's DC-facing IP
-    dc.cmd('ip route add 10.0.3.0/24 via 10.0.100.13') # Reach CC3 via cc3's DC-facing IP
+    for i in range(1, 9):
+        h = net.get(f'h1{i}')
+        if i<=4:
+            h.cmd('ip route add default via 10.0.1.200')
+        else:
+            h.cmd('ip route add default via 10.0.1.201')
 
-    # time.sleep(2)
-    # net.pingAll()
+    for i in range(1, 9):
+        h = net.get(f'h2{i}')
+        if i<=4:
+            h.cmd('ip route add default via 10.0.2.200')
+        else:
+            h.cmd('ip route add default via 10.0.2.201')
+    
+    for i in range(1, 9):
+        h = net.get(f'h3{i}')
+        if i <= 4:
+            h.cmd('ip route add default via 10.0.3.200')
+        else:
+            h.cmd('ip route add default via 10.0.3.201')
+    
+    r1.cmd('ip route add 10.0.3.0/24 via 10.0.2.202')  # r1 sends 10.0.3 traffic to r2
+    r2.cmd('ip route add 10.0.1.0/24 via 10.0.2.200')  # r2 sends 10.0.1 traffic to r1
+
+
+    
+
+    dc.cmd('ip route add 10.0.1.0/24 via 10.0.100.11')
+    dc.cmd('ip route add 10.0.2.0/24 via 10.0.100.12')
+    dc.cmd('ip route add 10.0.3.0/24 via 10.0.100.13')
+
+    time.sleep(2)
+  
+    
+
 
 
     
@@ -235,51 +273,51 @@ def simpleTest():
 
 
 
-    h1_cc1.cmd('iperf -c ' + h2_cc1.IP() + ' -u -b 1k &')
+    h1_cc1.cmd('iperf -c ' + h2_cc2.IP() + ' -u -b 1k &')
     time.sleep(1)
     h3_cc1.cmd('iperf -c ' + h2_cc2.IP() + ' -u -b 1k &')
     time.sleep(1)
-    h4_cc1.cmd('iperf -c ' + h2_cc3.IP() + ' -u -b 1k &')
+    h4_cc1.cmd('iperf -c ' + h2_cc2.IP() + ' -u -b 1k &')
     time.sleep(1)
-    h5_cc1.cmd('iperf -c ' + h2_cc1.IP() + ' -u -b 1k &')
+    h5_cc1.cmd('iperf -c ' + h2_cc2.IP() + ' -u -b 1k &')
     time.sleep(1)
     h6_cc1.cmd('iperf -c ' + h2_cc2.IP() + ' -u -b 1k &')
     time.sleep(1)
-    h7_cc1.cmd('iperf -c ' + h2_cc3.IP() + ' -u -b 1k &')
+    h7_cc1.cmd('iperf -c ' + h2_cc2.IP() + ' -u -b 1k &')
     time.sleep(1)
-    h8_cc1.cmd('iperf -c ' + h2_cc1.IP() + ' -u -b 1k &')
+    h8_cc1.cmd('iperf -c ' + h2_cc2.IP() + ' -u -b 1k &')
     time.sleep(1)
 
     
-    h1_cc2.cmd('iperf -c ' + h2_cc2.IP() + ' -u -b 1k &')
+    h1_cc2.cmd('iperf -c ' + h2_cc3.IP() + ' -u -b 1k &')
     time.sleep(1)
-    h3_cc2.cmd('iperf -c ' + h2_cc1.IP() + ' -u -b 1k &')
+    h3_cc2.cmd('iperf -c ' + h2_cc3.IP() + ' -u -b 1k &')
     time.sleep(1)
     h4_cc2.cmd('iperf -c ' + h2_cc3.IP() + ' -u -b 1k &')
     time.sleep(1)
-    h5_cc2.cmd('iperf -c ' + h2_cc2.IP() + ' -u -b 1k &')
+    h5_cc2.cmd('iperf -c ' + h2_cc3.IP() + ' -u -b 1k &')
     time.sleep(1)
-    h6_cc2.cmd('iperf -c ' + h2_cc1.IP() + ' -u -b 1k &')
+    h6_cc2.cmd('iperf -c ' + h2_cc3.IP() + ' -u -b 1k &')
     time.sleep(1)
     h7_cc2.cmd('iperf -c ' + h2_cc3.IP() + ' -u -b 1k &')
     time.sleep(1)
-    h8_cc2.cmd('iperf -c ' + h2_cc2.IP() + ' -u -b 1k &')
+    h8_cc2.cmd('iperf -c ' + h2_cc3.IP() + ' -u -b 1k &')
     time.sleep(1)
 
     
-    h1_cc3.cmd('iperf -c ' + h2_cc3.IP() + ' -u -b 1k &')
+    h1_cc3.cmd('iperf -c ' + h2_cc1.IP() + ' -u -b 1k &')
     time.sleep(1)
     h3_cc3.cmd('iperf -c ' + h2_cc1.IP() + ' -u -b 1k &')
     time.sleep(1)
-    h4_cc3.cmd('iperf -c ' + h2_cc2.IP() + ' -u -b 1k &')
+    h4_cc3.cmd('iperf -c ' + h2_cc1.IP() + ' -u -b 1k &')
     time.sleep(1)
-    h5_cc3.cmd('iperf -c ' + h2_cc3.IP() + ' -u -b 1k &')
+    h5_cc3.cmd('iperf -c ' + h2_cc1.IP() + ' -u -b 1k &')
     time.sleep(1)
     h6_cc3.cmd('iperf -c ' + h2_cc1.IP() + ' -u -b 1k &')
     time.sleep(1)
-    h7_cc3.cmd('iperf -c ' + h2_cc2.IP() + ' -u -b 1k &')
+    h7_cc3.cmd('iperf -c ' + h2_cc1.IP() + ' -u -b 1k &')
     time.sleep(1)
-    h8_cc3.cmd('iperf -c ' + h2_cc3.IP() + ' -u -b 1k &')
+    h8_cc3.cmd('iperf -c ' + h2_cc1.IP() + ' -u -b 1k &')
     time.sleep(1)
 
 
@@ -293,8 +331,10 @@ def simpleTest():
 
     enhanced_router1 = EnhancedRouter(r1, parameters={})
     enhanced_router2 = EnhancedRouter(r2, parameters={})
+    enhanced_router3 = EnhancedRouter(r2, parameters={})
     enhanced_router1.start()
     enhanced_router2.start()
+    # enhanced_router3.start()
 
     # enhanced_firewall1 = EnhancedFirewall(fw1,parameters={})
     # enhanced_firewall1.start()
@@ -319,10 +359,12 @@ def simpleTest():
                 cc1.cmd('kill %tcpdump')
                 cc2.cmd('kill %tcpdump')
                 cc3.cmd('kill %tcpdump')
-                # Read the file cc1.pcap and send a "Hello" message to the dc from cc
-                out = os.system('python3 packet_processor.py cc1.pcap cc1 &')
-                out1 = os.system('python3 packet_processor.py cc2.pcap cc2 &')
-                out2 = os.system('python3 packet_processor.py cc3.pcap cc3 &')
+
+                cc1.cmd('python3 packet_processor.py cc1.pcap cc1 &')
+                # print(cc1.cmd('cat output.log'))
+
+                cc2.cmd('python3 packet_processor.py cc2.pcap cc2 &')
+                cc3.cmd('python3 packet_processor.py cc3.pcap cc3 &')
                 # Send the data to the dc
                 cc1.cmd('cat cc1_payload.txt | nc -u -w 1 {} 12345'.format(dc.IP())) # send data as UDP
                 print(f"cc1: Sent UDP packet at {time.time()}")
@@ -347,9 +389,6 @@ def simpleTest():
                 dc.cmd('python3 dc_packet_saver.py capture.pcap')
                 # print PROCESSING
                 print("PROCESSING at dc")
-                # generate the attack report
-                dc.cmd('python3 ../detect_attack.py --csv dc_data.csv')
-                print("PROCESSING attack")
                 # delete the file
                 dc.cmd('rm capture.pcap')
                 # Begin the tcpdump of dc
@@ -381,6 +420,9 @@ def simpleTest():
             enhanced_router2.send_health_parameters(cc2)
             print(f"Router 2 sent health parameters to cc2")
             time.sleep(0.1)
+            # enhanced_router3.send_health_parameters(cc3)
+            # print(f"Router 3 sent health parameters to cc3")
+            # time.sleep(0.1)
 
             enhanced_router1.send_routing_parameters(cc1)
             print(f"Router 1 sent routing parameters to cc1")
@@ -388,6 +430,9 @@ def simpleTest():
             enhanced_router2.send_routing_parameters(cc2)
             print(f"Router 2 sent routing parameters to cc2")
             time.sleep(0.1)
+            # enhanced_router3.send_routing_parameters(cc3)
+            # print(f"Router 3 sent routing parameters to cc3")
+            # time.sleep(0.1)
 
             # enhanced_firewall1.send_firewall_parameters(cc1)
             # print(f"Router 3 sent firewall parameters to cc1")
